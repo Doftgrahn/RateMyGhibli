b3ab4f3d40bcc5e1ffb475f493d1c57f4aba704c
Ieva Lundin <eiwulia@gmail.com>
2019-04-01 13:42:30 +0200

    add toplist component

---
+++ src/app/toplist/toplist/toplist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toplist',
+  templateUrl: './toplist.component.html',
+  styleUrls: ['./toplist.component.scss']
+})
+export class ToplistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
