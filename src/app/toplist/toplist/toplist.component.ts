91a58928a9b63523e05c393311fbfb187bf7f32f
Ieva Lundin <eiwulia@gmail.com>
2019-04-02 16:36:39 +0200

    added pictures in the list and assets, made list show in order and order numbers in the list

--- src/app/toplist/toplist/toplist.component.ts
+++ src/app/toplist/toplist/toplist.component.ts
@@ -11,27 +11,53 @@ import { Film } from '../film';
 export class ToplistComponent implements OnInit {
 
   constructor(private filmData: FilmsService) { }
- 
-  filmUrl: Film[] = null;
-  films = [];
-
-  sortByRating(films: Film[]): void {
-    films.sort(function(a, b) {
-      return b.rt_score - a.rt_score;
-    });
-    this.films = films  
-  }
-
-
+  
+  films: Film[] = [];
 
   ngOnInit() { 
     this.filmData.getFilm()
-    .subscribe(data => this.films = data);
+    .subscribe(data => this.films = data.sort(function(a, b) {
+      return b.rt_score - a.rt_score;
+    })
+    )
   }
+
+  pictures = ['assets/pics/Only-Yesterday.jpg',
+    'assets/pics/The-tale-of-princess-kaguya.jpg',
+    'assets/pics/Grave-of-the-Fireflies.jpg',
+    'assets/pics/Spirited-Away.jpg',
+    'assets/pics/Kikis-Delivery-Service.jpg',
+    'assets/pics/Castle-in-the-Sky.jpg',
+    'assets/pics/ariety.jpg',
+    'assets/pics/porco-rosso.jpg',
+    'assets/pics/My-Neighbor-Totoro.jpg',
+    'assets/pics/Princess-Mononoke.jpg',
+    'assets/pics/Ponyo.jpg',
+    'assets/pics/When-Marnie-Was-There.jpg',
+    'assets/pics/Whisper-of-the-Heart.jpg',
+    'assets/pics/The-Cat-Returns.jpeg',
+    'assets/pics/the-wind-rises.jpg',
+    'assets/pics/Howls-Moving-Castle.jpg',
+    'assets/pics/From-up-on-poppy-hill.jpg',
+    'assets/pics/Pom-Poko.jpg',
+    'assets/pics/My-Neighbors-the-Yamadas.jpeg',
+    'assets/pics/Tales-from-Earthsea.jpg'
+  ];
+
   
+  //FUNCTIONs IF I WANT TO ADD LIST AND SORT IT ON A BUTTON
+  /* 
+  filmList: Film[] = null;
   getFilms() {
     this.filmData.getFilm()
-    .subscribe( film => {this.filmUrl = film;  console.log(this.filmUrl);} )
+    .subscribe( film => {this.filmList = film;  console.log(this.filmList);} )
+
+    sortByRating(filmList: Film[]): void {
+    films.sort(function(a, b) {
+      return b.rt_score - a.rt_score;
+    });
+    this.filmList = films;
   }
+  }*/
 }
 
