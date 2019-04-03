aecb6f23e5160d144a027c6e1ea72f6d5c1d50d0
Olga <tselyuk.olga@gmail.com>
2019-04-03 13:28:50 +0200

    form without validation, child component show-review has been created

--- src/app/add/add-review/add-review.component.ts
+++ src/app/add/add-review/add-review.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, AbstractControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-review',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-review.component.scss']
 })
 export class AddReviewComponent implements OnInit {
+  reviewForm: FormGroup = new FormGroup({
+		movieTitleControl: new FormControl('Leon', [Validators.required, Validators.minLength(1)]),
+		releaseYearControl: new FormControl([Validators.required]),
+    commentFieldControl: new FormControl([Validators.required]),
+    usersNameControl: new FormControl([Validators.required])
+		}
+	);
+  movieTitleControl: AbstractControl;
+  releaseYearControl: AbstractControl;
+  commentFieldControl: AbstractControl;
+  usersNameControl: AbstractControl;
 
   constructor() { }
 
---
+++ src/app/add/add-review/show-review/show-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-review',
+  templateUrl: './show-review.component.html',
+  styleUrls: ['./show-review.component.scss']
+})
+export class ShowReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
