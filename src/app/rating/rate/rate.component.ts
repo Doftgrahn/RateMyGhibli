b9405ec0994874462504f2144da37fd2b0276156
Simon grahn <simon.grahn@gmail.com>
2019-04-03 13:27:33 +0200

    added search

--- src/app/rating/rate/rate.component.ts
+++ src/app/rating/rate/rate.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from '../../shared/data.service';
+
 @Component({
   selector: 'app-rate',
   templateUrl: './rate.component.html',
   styleUrls: ['./rate.component.scss']
 })
 export class RateComponent implements OnInit {
+  searchText: any;
+  films;
 
 
 
 
 
 
-  constructor() { }
+
+
+
+
+  constructor(private ds: DataService) {this.films = this.ds.heroes;}
 
   ngOnInit() { }
 
--- src/app/rating/rating.module.ts
+++ src/app/rating/rating.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RateComponent } from './rate/rate.component';
+import {FormsModule} from '@angular/forms';
+
+/*----*/
+import { Pipe, PipeTransform } from '@angular/core';
+
+
+
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [RateComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    Ng2SearchPipeModule,
   ],
   exports: [RateComponent]
 })
