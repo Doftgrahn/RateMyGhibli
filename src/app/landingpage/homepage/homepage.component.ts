48037e47737140dcc81f212e44d90bd05bc41acd
Kamphol Taeng-iam <kamphol_23@hotmail.se>
2019-04-08 15:48:11 +0200

    loop image

--- src/app/landingpage/homepage/homepage.component.ts
+++ src/app/landingpage/homepage/homepage.component.ts
@@ -7,12 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
    items: Array<any> = []
-  index: number = 1;
+   image: Array<any> = []
 
 
   constructor() {
 
-
+this.image = [
+  {url:'assets/pics/Howls-Moving-Castle.jpg'},
+  {url:'assets/pics/The-tale-of-princess-kaguya.jpg'},
+  {url:'assets/pics/Grave-of-the-Fireflies.jpg'},
+  {url:'assets/pics/Spirited-Away.jpg'},
+  {url:'assets/pics/Kikis-Delivery-Service.jpg'},
+  {url:'assets/pics/Castle-in-the-Sky.jpg'},
+  {url:'assets/pics/ariety.jpg'},
+  {url:'assets/pics/porco-rosso.jpg'},
+  {url:'assets/pics/My-Neighbor-Totoro.jpg'},
+  {url:'assets/pics/the-wind-rises.jpg'},
+  {url:'assets/pics/When-Marnie-Was-There.jpg'},
+  {url:'assets/pics/Whisper-of-the-Heart.jpg'},
+  {url:'  assets/pics/My-Neighbors-the-Yamadas.jpeg'},
+  {url:'assets/pics/Tales-from-Earthsea.jpg'},
+  {url:'assets/pics/Princess-Mononoke.jpg'}
+];
 
      this.items =[
       {name: 'assets/pics/Princess-Mononoke.jpg'},
