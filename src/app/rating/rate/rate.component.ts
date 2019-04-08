63308ddd28a0d1b03bd8fba071e211ad7f863b10
Simon grahn <simon.grahn@gmail.com>
2019-04-08 09:48:08 +0200

    done with shit

--- src/app/rating/rate/rate.component.ts
+++ src/app/rating/rate/rate.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FilmsService } from '../../shared/films.service';
-import { Film } from '../../shared/film';
+
+import { FilmsService } from '../../shared/films.service'; /* Service */
+
+import { Film } from '../../shared/film';    /* Interfaces */
+import { Rate } from '../../shared/rate';
 
 @Component({
   selector: 'app-rate',
@@ -14,36 +17,52 @@ export class RateComponent implements OnInit {
   films: Film[] = [];      /*Get Film*/
   ratingClicked: Film[];
   itemIdRatingClicked: string;
-  getRated: any;
-  state: any = {
-    rated: []
+
+  state = {
+    rated: null
   };
-  parsedItems: any;
+
 
 
   ratingComponentClick(clickObj: any): void {
     const item = this.films.find(((i: any) => i.id === clickObj.id));
     if (!!item) {
+      // There is a Ghibli movie with this id (this.films)
       item.rating = clickObj.rating;
       this.ratingClicked = clickObj.rating;
       this.itemIdRatingClicked = item.title;
     }
-    this.state.rated.push({
-      id: clickObj.id,
-      rating: clickObj.rating
-    })
+    if (this.state.rated.find(rating => rating.id === clickObj.id)) {
+      this.state.rated.forEach(r => {
+        if (r.id === clickObj.id) {
+          r.rating = clickObj.rating;
+        }
+      })
+    } else {
+      // There is no rating for clickObj.id  (this.state.rated)
+      this.state.rated.push({
+        id: clickObj.id,
+        rating: clickObj.rating
+      })
+    }
     localStorage.setItem('rating', JSON.stringify(this.state.rated));
   }
 
-
+  // undefined är "falsy", precis som 0, '', null
+  // !undefined -> true
   ngOnInit() {
-    this.getRated = localStorage.getItem('rating');
-    this.parsedItems = JSON.parse(this.getRated);
-
+    let playList: string = localStorage.getItem('rating');
+    if (!playList) {
+      // playlist är undefined, dvs det finns inget i localStorage
+      this.setupLocalStorage()
+      playList = '[]';
+    }
+    const parsedItems: Rate[] = JSON.parse(playList);
+    this.state.rated = parsedItems;
     this.filmData.getFilm().subscribe(ghibliData => {
       this.films = ghibliData.map(e => {
 
-        let oldRating = this.parsedItems.find(function(item: any) {
+        let oldRating: Rate = parsedItems.find(function(item: Rate) {
           return item.id === e.id;
         });
 
@@ -53,6 +72,11 @@ export class RateComponent implements OnInit {
       })
     })
   }
+
+  setupLocalStorage() {
+    localStorage.setItem('rating', JSON.stringify(this.state.rated))
+  }
+
 
   constructor(private filmData: FilmsService) { }
 
--- src/app/rating/rate/stars/stars.component.ts
+++ src/app/rating/rate/stars/stars.component.ts
@@ -25,4 +25,8 @@ export class StarsComponent implements OnInit {
     })
   }
 
+  resetRating() {
+    this.onClick(null);
+  }
+
 }
