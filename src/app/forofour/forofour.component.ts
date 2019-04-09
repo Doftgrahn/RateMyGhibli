5036674519a362a1b17231e0eb027b2d09ca451d
Simon grahn <simon.grahn@gmail.com>
2019-04-09 15:09:18 +0200

    added forourfor

---
+++ src/app/forofour/forofour.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forofour',
+  templateUrl: './forofour.component.html',
+  styleUrls: ['./forofour.component.scss']
+})
+export class ForofourComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
