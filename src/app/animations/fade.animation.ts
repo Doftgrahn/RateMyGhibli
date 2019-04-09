59c72c7e3d26e0d47918e5232a4226fb04bfe136
Simon grahn <simon.grahn@gmail.com>
2019-04-09 13:50:52 +0200

    animations done

--- src/app/animations/fade.animation.ts
+++ src/app/animations/fade.animation.ts
@@ -12,14 +12,14 @@ export const fadeAnimation =
       query(':leave',
         [
           style({ opacity: 1 }),
-          animate('0.5s', style({ opacity: 0 }))
+          animate('0.2s', style({ opacity: 0 }))
         ],
         { optional: true }
       ),
       query(':enter',
         [
           style({ opacity: 0 }),
-          animate('0.5s', style({ opacity: 1 }))
+          animate('0.2s', style({ opacity: 1 }))
         ],
         { optional: true }
       )
