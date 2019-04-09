199cd944936fc5b952be891b470fb1b7ed3592fd
Kamphol Taeng-iam <kamphol_23@hotmail.se>
2019-04-09 22:56:04 +0200

    gonw with all shity code (I hope)

--- src/app/about/about/about.component.ts
+++ src/app/about/about/about.component.ts
@@ -6,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
-
+members: Array<any> = [];
+  constructor() {
+this.members = [
+  {image:'assets/profile_pic/Kampis.jpg', name:'Kmaphol Taeng-Iam', git:'https://github.com/kamphol23', mail:'kamphol_23@hotmail.se', linkedin:'https://www.linkedin.com/in/kamphol-taeng-iam-759627173/' },
+  {image:'assets/profile_pic/Olga.jpg', name:'Olga Tselyuk', git:'', mail:'', linkedin:'https://www.linkedin.com/in/olga-tselyuk-243456104/' },
+  {image:'assets/profile_pic/Simon.jpeg', name:'Simon Grahn', git:'https://github.com/Doftgrahn', mail:'', linkedin:'https://www.linkedin.com/in/simon-grahn-06994797/' },
+  {image:'assets/profile_pic/Ieva.jpg', name:'Ieva', git:'https://github.com/eiwulia', mail:'', linkedin:'https://www.linkedin.com/in/ieva-lundin-bba94a171/' }
+];
+}
   ngOnInit() {
   }
 
