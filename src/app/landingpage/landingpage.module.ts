16d3431f78c91f904bd3181c8d75e35b1f0b75f0
Simon grahn <simon.grahn@gmail.com>
2019-04-10 13:48:18 +0200

    fix routes in landingpage

--- src/app/landingpage/landingpage.module.ts
+++ src/app/landingpage/landingpage.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage/homepage.component';
 import { Ng2CarouselamosModule } from 'ng2-carouselamos';
+import { AppRoutingModule } from '../shared/app-routing.module';
+
 @NgModule({
   declarations: [HomepageComponent],
   imports: [
     CommonModule,
-     Ng2CarouselamosModule
+    Ng2CarouselamosModule,
+    AppRoutingModule,
   ]
 })
-export class LandingpageModule {}
+export class LandingpageModule { }
